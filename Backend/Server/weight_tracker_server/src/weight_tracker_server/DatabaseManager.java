f5a3037093c8e4f3b0f273ebf6eae841fd81d4cb
Joshua Manascu <joshuamanascu@ymail.com>
2026-03-30 14:07:22 -0400

    Removed unused imports

--- Backend/Server/weight_tracker_server/src/weight_tracker_server/DatabaseManager.java
+++ Backend/Server/weight_tracker_server/src/weight_tracker_server/DatabaseManager.java
@@ -1,12 +1,8 @@
 package weight_tracker_server;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
